3576b35880a7ea96a30bab0d059aeca40623cd6e
qwer <igordeev2006daniil@gmail.com>
2026-05-21 19:49:18 +0600

    commit

--- yatube_api/api/admin.py
+++ yatube_api/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from posts.models import Comment, Group, Post
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Group)
+admin.site.register(Comment)
